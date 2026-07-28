f69a51b312d167a62e0cae86b839183c82bb8a53
ahmed <mr. Ahmedsharkawy@gmail.com>
2026-07-28 19:04:00 +0400

    Update site

--- java/sw-refresh.js
+++ java/sw-refresh.js
@@ -1,14 +1,14 @@
 (function () {
   if (!('serviceWorker' in navigator)) return;
 
-  var REFRESH_KEY = 'sw-v206-global-refresh';
+  var REFRESH_KEY = 'sw-v208-global-refresh';
   var refreshed = false;
 
   function clearOldCaches() {
     if (!('caches' in window)) return;
     caches.keys().then(function (keys) {
       keys.forEach(function (key) {
-        if (key !== 'bariq-v222') caches.delete(key).catch(function () {});
+        if (key !== 'bariq-v224') caches.delete(key).catch(function () {});
       });
     }).catch(function () {});
   }
@@ -64,7 +64,7 @@
     markRefreshed();
   });
 
-  navigator.serviceWorker.register('/sw.js?v=206', { updateViaCache: 'none' }).then(function (reg) {
+  navigator.serviceWorker.register('/sw.js?v=208', { updateViaCache: 'none' }).then(function (reg) {
     clearOldCaches();
     requestActivation(reg);
     sendPushLanguage(reg);
