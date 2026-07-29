e5d7c4924a6cb2057ef7b7a49162c09adf5a9be3
ahmed <mr. Ahmedsharkawy@gmail.com>
2026-07-29 20:08:08 +0400

    Update site

--- java/sw-refresh.js
+++ java/sw-refresh.js
@@ -1,14 +1,14 @@
 (function () {
   if (!('serviceWorker' in navigator)) return;
 
-  var REFRESH_KEY = 'sw-v244-global-refresh';
+  var REFRESH_KEY = 'sw-v247-global-refresh';
   var refreshed = false;
 
   function clearOldCaches() {
     if (!('caches' in window)) return;
     caches.keys().then(function (keys) {
       keys.forEach(function (key) {
-        if (key !== 'bariq-v260') caches.delete(key).catch(function () {});
+        if (key !== 'bariq-v263') caches.delete(key).catch(function () {});
       });
     }).catch(function () {});
   }
@@ -64,7 +64,7 @@
     markRefreshed();
   });
 
-  navigator.serviceWorker.register('/sw.js?v=244', { updateViaCache: 'none' }).then(function (reg) {
+  navigator.serviceWorker.register('/sw.js?v=247', { updateViaCache: 'none' }).then(function (reg) {
     clearOldCaches();
     requestActivation(reg);
     sendPushLanguage(reg);
--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Service Worker - Bariq PWA */
-const CACHE = 'bariq-v260';
+const CACHE = 'bariq-v263';
 let _badgeCount = 0;
 const STATIC_URLS = [
   '/',
